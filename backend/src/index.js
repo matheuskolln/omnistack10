0345eb2b9b20b7f2bd5c062b0e820b4353db3965
Matheus Kolln Nagildo <matheuzhenrik@gmail.com>
2020-01-16 01:08:49 -0300

    aula 3

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes')
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://matheuskolln:Euteamos4@cluster0-s2cwv.mongodb.ne
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
